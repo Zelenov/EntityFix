83d9e27b575a6aa7685588b428fbb18548fb48ce
Eugene <nN6b7NNJFch2>
2016-04-19 13:01:46 +0300

    AssemblyInfo

--- EntityFix/EntityFix/Properties/AssemblyInfo.cs
+++ EntityFix/EntityFix/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityFix")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fixes EF EntityCommandExecutionException")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Evgeny Zelenov")]
 [assembly: AssemblyProduct("EntityFix")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Evgeny Zelenov, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
